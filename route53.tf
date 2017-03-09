17f8b4ed90460ac436dbdfbeab2d50f1aaa534cb
J. Matt Peterson <jmatt@jmatt.org>
2017-03-09 12:57:30 -0700

    Add DNS entries for site/local IPs.
    	modified:   route53.tf

--- route53.tf
+++ route53.tf
@@ -21,3 +21,22 @@ resource "aws_route53_record" "rabbit_numbered_route53" {
   ttl = "300"
   records = ["${element(openstack_compute_instance_v2.rabbit_instances.*.access_ip_v4, count.index)}"]
 }
+
+# Add rabbit-site.lsst.codes to rabbit-site-0.lsst.codes
+resource "aws_route53_record" "rabbit_site_route53" {
+  zone_id = "Z3TH0HRSNU67AM"
+  name = "rabbit-site.lsst.codes"
+  type = "A"
+  ttl = "300"
+  records = ["${openstack_compute_instance_v2.rabbit_instances.0.network.0.fixed_ip_v4}"]
+}
+
+# Add rabbit-site-#.lsst.codes
+resource "aws_route53_record" "rabbit_site_numbered_route53" {
+  count = "${var.count}"
+  zone_id = "Z3TH0HRSNU67AM"
+  name = "rabbit-site-${count.index}.lsst.codes"
+  type = "A"
+  ttl = "300"
+  records = ["${element(openstack_compute_instance_v2.rabbit_instances.*.network.0.fixed_ip_v4, count.index)}"]
+}
